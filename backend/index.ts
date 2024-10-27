6e5f52eeb828198aa1d20cc8ff3a8c534bec2c0b
Louafi Razine <louafi.razine@gmail.com>
2024-10-27 18:16:27 +0100

    update index with datatbase / server ready / and security protections / singleton to prevent multiple connexion to db

--- backend/index.ts
+++ backend/index.ts
@@ -1,19 +1,47 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import mongoSanitize from "express-mongo-sanitize";
+import helmet from "helmet";
+import UserRoutes from "./routes/user";
+import ProductRoutes from "./routes/product";
+// @ts-ignore
+import { xss } from "express-xss-sanitizer";
+import connectDB from "./helpers/database";
 
 const app = express();
 
-app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
 app.use(express.json());
+app.use(cors());
+app.use(UserRoutes);
+app.use(ProductRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello from api server:" });
 });
 
-app.listen(process.env.SERVER_PORT || 3000, () => {
-  console.log(
-    `App listening at <http://localhost>:${process.env.SERVER_PORT} `
-  );
+app.all("*", (req: Request, res: Response) => {
+  res.status(404).send("Page introuvable");
 });
+
+const startServer = async () => {
+  try {
+    await connectDB(); // Establish the database connection
+
+    // @ts-ignore
+    const PORT = process.env.SERVER_PORT || 8080;
+
+    app.listen(PORT, () => {
+      `App listening at <http://localhost>:${PORT} `;
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1); // Exit the process with failure
+  }
+};
+
+startServer();
 
 module.exports = app;
