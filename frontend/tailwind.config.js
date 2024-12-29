0feac27b9f824e2e5c38112c7166dd60a94d9459
Louafi Razine <louafi.razine@gmail.com>
2024-12-29 21:48:46 +0100

    added flowbite / added const as env var  temp solution / removed readme in frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}', './node_modules/flowbite/**/*.js'],
   theme: {
     extend: {
       colors: {
         primary: '#1317F7',
         secondary: '#2B3FC4',
         tertiary: '#3C3744'
-      },
+      }
     }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ]
 }
